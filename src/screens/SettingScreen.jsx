29684f78e02981e0e21945ef27cd9e3ee2152eac
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-12-03 16:00:44 -0700

    Implement some test settings

--- src/screens/SettingScreen.jsx
+++ src/screens/SettingScreen.jsx
@@ -1,25 +1,61 @@
-import React from "react";
-import { Layout, Text, Button } from "@ui-kitten/components";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useState, useEffect } from 'react';
+import { Layout, Text, Input, Button, Switch } from '@ui-kitten/components';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { TouchableWithoutFeedback, Keyboard } from 'react-native';
 
-const SettingScreen = ({ onLogout }) => {
-  const handleLogout = async () => {
-    await AsyncStorage.removeItem("isLoggedIn");
-    onLogout();
+const SettingScreen = () => {
+  const [username, setUsername] = useState('');
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+
+  useEffect(() => {
+    const loadSettings = async () => {
+      const storedUsername = await AsyncStorage.getItem('username');
+      const storedNotificationsEnabled = await AsyncStorage.getItem('notificationsEnabled');
+
+      setUsername(storedUsername || '');
+      setNotificationsEnabled(storedNotificationsEnabled === 'true');
+    };
+
+    loadSettings();
+  }, []);
+
+  const saveSettings = async () => {
+    await AsyncStorage.setItem('username', username);
+    await AsyncStorage.setItem('notificationsEnabled', String(notificationsEnabled));
+
+    console.log('Settings saved:', { username, notificationsEnabled });
   };
 
   return (
-    <Layout
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 30,
-      }}
-    >
-      <Text category="h1">Setting</Text>
-      <Button onPress={handleLogout}>Logout</Button>
-    </Layout>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <Layout
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 30,
+        }}
+      >
+        <Text category="h1">Settings</Text>
+
+        <Input
+          value={username}
+          placeholder="Enter your username"
+          onChangeText={setUsername}
+          style={{ marginVertical: 10 }}
+        />
+
+        <Layout style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 10 }}>
+          <Text category="label">Enable Notifications:</Text>
+          <Switch
+            checked={notificationsEnabled}
+            onChange={() => setNotificationsEnabled(!notificationsEnabled)}
+          />
+        </Layout>
+
+        <Button onPress={saveSettings}>Save Settings</Button>
+      </Layout>
+    </TouchableWithoutFeedback>
   );
 };
 
